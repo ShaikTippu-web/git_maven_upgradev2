110593b644a44f12d388bb959b022b0b2638b414
Shaik Noormahammad <Shaikux91@gmail.com>
2023-06-22 22:34:24 +0530

    change in code

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -37,16 +37,10 @@ public class TestGreeter {
 
     @Test
   public void greetShouldIncludeGreetingMessage() {
-    String someone = "World, Thanks you making me Happy";
+    String someone = "World, Thank you this is Shaik";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
   
-  @Test
-  public void greetShouldIncludeGreetingMessage() {
-    String someone = "World, This is shaik";
-
-    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
-  }
 }
 
